5695c059ea3329bd88ca35e4a0ebfa3d580afc2d
Stephen Copeland <knkp934@gmail.com>
2015-04-13 10:07:49 -0400

    solved int to bytes on client.py and byte to int on server.py, note currently they are working on loopback. Need to add this all to class

--- client.py
+++ client.py
@@ -4,24 +4,30 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 local_host = socket.gethostname()
-server_address = ('52.0.129.208', 5001)
+#server_address = ('52.0.129.208', 5001)
+server_address = ('127.0.0.1', 5001)
 print('connecting to %s port %s',server_address)
 
 sock.connect(server_address)
 
 try:
-        message = b'This is a message... for a faggot'
-        print('sending %s',message)
-        sock.sendall(message)
-        amount_received = 0
-        amount_expected = len(message)
+        lenlist = []
+        length = len(socket.gethostbyname(socket.gethostname()))
+        #message = b'This is a message... for a faggot'
+        print(length)
+        lenlist.append(length)
+        length = bytes(lenlist)
+        print(length)
+        sock.sendall(length)
+       # amount_received = 0
+       # amount_expected = len(message)
 
 ## need to fix encoding issue, needs to be converted to bytes from int before sent -SC 4/13/2015
 
-        while amount_received < amount_expected:
-                data = sock.recv(16)
-                amount_received += len(data)
-                print('received %s',data)
+        #while amount_received < amount_expected:
+         #       data = sock.recv(16)
+          #      amount_received += len(data)
+           #     print('received %s',data)
 finally:
         print('closing socket')
         sock.close()
--- server.py
+++ server.py
@@ -1,29 +1,30 @@
 import socket
 import sys
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('',5001)
+server_address = ('127.0.0.1', 5001)
 sock.bind(server_address)
 sock.listen(1)
 
 while True:
-		print('waiting for a connection')
-		connection, client_address = sock.accept()
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
 
-		try:
-				print('connection from', client_address)
+    print('connection from', client_address)
 
-				while True:
-						data = connection.recv(16)
-						print('received %s', data)
+    while True:
+        data = connection.recv(2)
+        if len(data)>0:
+            data = int.from_bytes(data, byteorder='little')
+            print(data)
 
-						if data:
-								print('sending data back to the client')
-								connection.sendall(data)
-						else:
-								print('no more data from client')
-								break
+# if data:
+#			print('sending data back to the client')
+#			connection.sendall(data)
+#	else:
+#			print('no more data from client')
+#			break
 
-		finally:
-				connection.close()
+#	finally:
+#			connection.close()
